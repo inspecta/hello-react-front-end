8dda7a81f8f6f3b56fef7a92d85bf0f6f5ece7ec
Derrick <djmulinde@gmail.com>
2023-02-21 14:29:21 +0300

    Add redux store

---
+++ src/redux/features/slice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import types from '../types/'
+
+const url = 'http://localhost:3000/api/v1/messages';
+
+export const fetchGreeting = createAsyncThunk(
+  types.FETCH_GREETING,
+  async () => {
+    const response = await axios.get(url);
+    return response.data;
+  },
+);
+
+// Initial state
+const initialState = {
+  greetings: [],
+  error: null,
+  status: 'idle',
+};
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {
+    greeting(state, action) {
+      state.greetings = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => {
+        console.log('Im here');
+        state.status = 'loading';
+      })
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.greetings = action.payload;
+      })
+      .addCase(fetchGreeting.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { greeting } = greetingsSlice.actions;
+export default greetingsSlice.reducer;
+

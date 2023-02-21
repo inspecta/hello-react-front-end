4c264e3d53c3e27f5244b912e46d66fcd89fdd4d
Derrick <djmulinde@gmail.com>
2023-02-21 14:28:52 +0300

    Add components

---
+++ src/components/Greetings.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchGreeting } from '../redux/features/slice';
+
+const Greetings = () => {
+  const dispatch = useDispatch();
+
+  const fetchedGreeting = useSelector((state) => state.greetings)
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch])
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <p>
+        {fetchedGreeting.greetings
+          ? <p>{ }fetchedGreeting.greetings.greetings</p>
+          : <p>No greetings available</p>
+        }
+      </p>
+    </div>
+  )
+}
+
+export default Greetings
